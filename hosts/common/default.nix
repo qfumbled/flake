20b7af83d18164fb8765c5ec43b5e06aad7f30bf
qfumbled <q.fumbled@proton.me>
2025-07-19 10:54:23 -0400

    Create default.nix

---
+++ hosts/common/default.nix
@@ -0,0 +1,14 @@
+{...}: {
+  imports = [
+    ./audio.nix
+    ./boot.nix
+    ./locales.nix
+    ./flatpak.nix
+    ./misc.nix
+    ./networking.nix
+    ./nixpkgs.nix
+    ./plasma.nix
+    ./optimize.nix
+    ./bluetooth.nix
+  ];
+}
