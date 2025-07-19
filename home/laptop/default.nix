3269bb8da4a7d759f9fabc30bddf93eb178ffe3b
qfumbled <q.fumbled@proton.me>
2025-07-19 14:30:24 -0400

    Update default.nix

--- home/laptop/default.nix
+++ home/laptop/default.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   imports = [
     ../common
-    ./plasma.nix
+    ./plasma.2.nix
   ];
-
+  # normal plasma is plasma.nix plasma.2.nix if you wanna be the same as desktop
   home.packages = with pkgs; [
   ];
 }
