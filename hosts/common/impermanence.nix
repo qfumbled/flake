0c1519ec639bbb17abfbd5918537bb9536fe9add
qfumbled <monegasque>
2025-08-05 11:51:50 -0400

    added file also fixed bad symlink loop

--- hosts/common/impermanence.nix
+++ hosts/common/impermanence.nix
@@ -1,9 +1,13 @@
-{ lib, config, ... }:
 {
+  lib,
+  config,
+  ...
+}: {
   config = {
     users.users.monaco = {
-      hashedPasswordFile = "$6$/d.pgzfuTq2metTH$4t.IIo6H46OAbhpnmon18yIedo6Iz41NgrGiup4tBG8DCW3VOQCRzt.Fldz1077mIRa/QWv2yQw1unWCx5Lxt/";
+      isNormalUser = true;
       uid = 1000;
+      hashedPasswordFile = "/persist/passwords/user";
     };
 
     environment.persistence."/persist" = {
@@ -14,11 +18,28 @@
         "/var/lib/tailscale"
         "/var/lib/systemd/coredump"
         "/var/log"
+        "/var/lib/nixos"
+        "/persist/passwords"
       ];
       files = [
         "/etc/machine-id"
         "/etc/wg.key"
+        "/persist/passwords/user"
       ];
+    };
+
+    system.activationScripts.createPasswordFile = {
+      deps = [];
+      text = ''
+        echo "[+] Ensuring password file exists at /persist/passwords/user"
+        install -d -m 700 -o root -g root /persist/passwords
+        if [ ! -f /persist/passwords/user ]; then
+          echo '[+] Creating password file'
+          echo '$6$/d.pgzfuTq2metTH$4t.IIo6H46OAbhpnmon18yIedo6Iz41NgrGiup4tBG8DCW3VOQCRzt.Fldz1077mIRa/QWv2yQw1unWCx5Lxt/' > /persist/passwords/user
+          chmod 600 /persist/passwords/user
+          chown root:root /persist/passwords/user
+        fi
+      '';
     };
   };
 }
