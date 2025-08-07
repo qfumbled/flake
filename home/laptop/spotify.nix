1c2e7a187d2adc48471d3df7d7a9daf50a8afb49
qfumbled <q.fumbled@proton.me>
2025-08-07 17:33:17 -0400

    forgot i

--- home/laptop/spotify.nix
+++ home/laptop/spotify.nix
@@ -4,7 +4,7 @@
   config,
   ...
 }: {
-  imports = [inputs.spicetify-nx.homeManagerModules.default];
+  imports = [inputs.spicetify-nix.homeManagerModules.default];
 
   programs.spicetify = let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
