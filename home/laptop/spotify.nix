18649210f10faf1b9d01fd22aa2b9fc894bb0610
qfumbled <q.fumbled@proton.me>
2025-08-07 18:41:21 -0400

    Update spotify.nix

--- home/laptop/spotify.nix
+++ home/laptop/spotify.nix
@@ -8,8 +8,21 @@
 
   programs.spicetify = let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in {
+    enable = true;
+    spotifyPackage = pkgs.spotify;
+    spicetifyPackage = pkgs.spicetify-cli;
 
-    test = {
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      shuffle
+    ];
+
+    theme = spicePkgs.themes.custom; # Explicitly use 'custom' theme
+
+    colorScheme = "custom"; # Set the color scheme to 'custom'
+
+    customColorScheme = {
       scheme = "test";
       author = "kevin";
       base00 = "f5f3ef";
@@ -29,59 +42,5 @@
       base0E = "9661cc";
       base0F = "aaa9a9";
     };
-
-    far = {
-      scheme = "far";
-      author = "Kevin";
-      base00 = "0f1314";
-      base01 = "151a1c";
-      base02 = "21282b";
-      base03 = "364044";
-      base04 = "a8a8a8";
-      base05 = "c2c4c2";
-      base06 = "c2c4c2";
-      base07 = "d4d9d4";
-      base08 = "b24957";
-      base09 = "d08666";
-      base0A = "95b971";
-      base0B = "6a9440";
-      base0C = "75ad99";
-      base0D = "598278";
-      base0E = "e7ae77";
-      base0F = "717171";
-    };
-  in {
-    enable = true;
-    spotifyPackage = pkgs.spotify;
-    spicetifyPackage = pkgs.spicetify-cli;
-
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      shuffle
-    ];
-
-    theme = {
-      name = "custom"; # Use 'custom' as the theme name
-      colors = {
-        base00 = test.base00;
-        base01 = test.base01;
-        base02 = test.base02;
-        base03 = test.base03;
-        base04 = test.base04;
-        base05 = test.base05;
-        base06 = test.base06;
-        base07 = test.base07;
-        base08 = test.base08;
-        base09 = test.base09;
-        base0A = test.base0A;
-        base0B = test.base0B;
-        base0C = test.base0C;
-        base0D = test.base0D;
-        base0E = test.base0E;
-        base0F = test.base0F;
-      };
-    };
-
-    colorScheme = test.scheme; # Switch between 'test' or 'far'
   };
 }
