72051e27f4017db6d651757b0b90116b3a0edc2e
qfumbled <q.fumbled@proton.me>
2025-08-07 20:25:32 -0400

    don't go too far with hardening but like basic hardening is enough

--- hosts/common/bootloader/default.nix
+++ hosts/common/bootloader/default.nix
@@ -17,23 +17,18 @@
       "udev.log_priority=3"
       "rd.systemd.show_status=auto"
 
-      # Security hardening
-      "lockdown=confidentiality" # Lock kernel from runtime modifications
-      "module.sig_enforce=1" # Require signed kernel modules
-      "rootfstype=xfs" # Limit FS to XFS
-      "debugfs=off" # Disable debugfs
-      "oops=panic" # Panic on kernel oops
-      "panic_on_oops=1" # Force immediate panic
-      "module_blacklist=cramfs,freevxfs,hfs,hfsplus,udf,ufs" # Disable unused FS
+      # Basic hardening
+      "rootfstype=xfs"
+      "module.sig_enforce=1"
+      "lockdown=confidentiality"
     ];
 
     initrd.verbose = false;
     consoleLogLevel = 3;
 
     kernel.sysctl = {
-      "kernel.kptr_restrict" = 2;
+      "kernel.kptr_restrict" = 1;
       "kernel.dmesg_restrict" = 1;
-      "kernel.unprivileged_bpf_disabled" = 1;
     };
   };
 }
