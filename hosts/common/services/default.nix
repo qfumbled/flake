5500bff412a5c73306fc2eae9c247e94c88904dd
qfumbled <q.fumbled@proton.me>
2025-08-08 18:42:38 -0400

    fix

--- hosts/common/services/default.nix
+++ hosts/common/services/default.nix
@@ -1,45 +1,41 @@
-{pkgs, ...}: {
-  boot = {
-    kernelPackages = pkgs.linuxPackages_hardened;
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption;
+in {
+  options.opt = {
+    pipewire = mkEnableOption "Enable PipeWire audio backend";
+    upower = mkEnableOption "Enable UPower service";
+  };
 
-    loader = {
-      systemd-boot.enable = true;
-      efi.efiSysMountPoint = "/boot";
-      efi.canTouchEfiVariables = true;
-      timeout = 15;
-    };
+  config = {
+    services = {
+      dbus.enable = true;
+      gvfs.enable = true;
+      fwupd.enable = true;
+      # Conditional upower service enablement
+      upower.enable = mkIf config.opt.upower true;
 
-    plymouth.enable = true;
-    kernelModules = ["v4l2loopback"];
-    extraModulePackages = [pkgs.linuxPackages_hardened.v4l2loopback];
-    kernelParams = [
-      "quiet"
-      "splash"
-      "fastboot"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-      "rootfstype=xfs"
-      "module.sig_enforce=1"
-      "lockdown=confidentiality"
-      "mitigations=on"
-      "ideapad_laptop.allow_v4_dytc=Y"
-      "nvme_core.default_ps_max_latency_us=0"
-    ];
+      logind = {
+        powerKey = "suspend";
+        lidSwitch = "suspend";
+        lidSwitchExternalPower = "lock";
+      };
 
-    initrd.verbose = false;
-    consoleLogLevel = 3;
+      pipewire = mkIf config.opt.pipewire {
+        enable = true;
+        alsa.enable = true;
+        pulse.enable = true;
+        jack.enable = false;
+      };
 
-    kernel.sysctl = {
-      "net.isoc" = true;
-      "kernel.kptr_restrict" = 1;
-      "kernel.dmesg_restrict" = 1;
-
-      "vm.swappiness" = 10;
-      "vm.vfs_cache_pressure" = 50;
-      "vm.dirty_ratio" = 10;
-      "vm.dirty_background_ratio" = 5;
-
-      "kernel.nmi_watchdog" = 0;
+      xserver.xkb = {
+        layout = "us";
+        options = "compose:rctrl,caps:escape";
+      };
     };
   };
 }
