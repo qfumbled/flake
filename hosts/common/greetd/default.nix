b8171d879cc847710907bb7323f54173065e0072
qfumbled <q.fumbled@proton.me>
2025-08-08 13:52:04 -0400

    saw lightdm instead of tuigreet

--- hosts/common/greetd/default.nix
+++ hosts/common/greetd/default.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+
+{
+  services.lightdm.enable = false;
+  services.gdm.enable = false;
+
+  # Enable greetd with tuigreet
   services.greetd = {
     enable = true;
     vt = 3;
     settings = {
       default_session = {
-        user = "monaco";
+        user = "monaco";  # your username
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd startplasma-wayland";
       };
     };
