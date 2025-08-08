9c7ac094a4eba1fd2125689c5be3e48ebbf711f6
qfumbled <q.fumbled@proton.me>
2025-08-07 20:17:25 -0400

    Update default.nix

--- hosts/common/bootloader/default.nix
+++ hosts/common/bootloader/default.nix
@@ -1,26 +1,42 @@
 {pkgs, ...}: {
   boot = {
-    boot.kernelPackages = pkgs.linuxPackages_hardened;
+    kernelPackages = pkgs.linuxPackages_hardened;
+
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       timeout = 15;
     };
 
-    plymouth = {
-      enable = false;
-    };
+    plymouth.enable = false;
 
     kernelParams = [
       "quiet"
       "splash"
       "fastboot"
-      "boot.shell_on_fail"
       "udev.log_priority=3"
       "rd.systemd.show_status=auto"
+
+      # Security hardening
+      "lockdown=confidentiality" # Lock kernel from runtime modifications
+      "module.sig_enforce=1" # Require signed kernel modules
+      "rootfstype=xfs" # Limit FS to XFS
+      "debugfs=off" # Disable debugfs
+      "oops=panic" # Panic on kernel oops
+      "panic_on_oops=1" # Force immediate panic
+      "module_blacklist=cramfs,freevxfs,hfs,hfsplus,udf,ufs" # Disable unused FS
     ];
 
     initrd.verbose = false;
     consoleLogLevel = 3;
+
+    kernel.sysctl = {
+      "kernel.kptr_restrict" = 2;
+      "kernel.dmesg_restrict" = 1;
+      "kernel.unprivileged_bpf_disabled" = 1;
+    };
   };
+
+  # Optional: Enable AppArmor for additional process confinement
+  security.apparmor.enable = true;
 }
