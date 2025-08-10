0e6fbc9346c9a4b3a7474656a8a0d8571045eb87
qfumbled <q.fumbled@proton.me>
2025-08-10 12:45:19 -0400

    Update theme.nix

--- home/laptop/hyprland/config/theme.nix
+++ home/laptop/hyprland/config/theme.nix
@@ -1,31 +1,35 @@
 { inputs, config, lib, ... }:
 
+let
+  # Define shadow settings in one place
+  shadowSettings = {
+    enabled = true;
+    range = 18;
+    render_power = 3;
+    # Force this color to avoid conflicting definitions
+    color = lib.mkForce "rgba(00000066)";
+  };
+
+  generalSettings = with config.lib.stylix.colors; {
+    gaps_in = 8;
+    gaps_out = 12;
+    border_size = 2;
+    "col.inactive_border" = "rgb(${base01})";
+    "col.active_border" = "rgb(${base01})";
+    layout = "dwindle";
+    resize_on_border = "true";
+  };
+in
 {
   wayland.windowManager.hyprland = {
     settings = {
-      # Set your own cursor theme here if you want
       env = [
         # "HYPRCURSOR_THEME,YourCursorThemeHere"
         "HYPRCURSOR_SIZE,24"
       ];
-
-      general = with config.lib.stylix.colors; {
-        gaps_in = 8;
-        gaps_out = 12;
-        border_size = 2;
-        "col.inactive_border" = "rgb(${base01})";
-        "col.active_border" = "rgb(${base01})";
-        layout = "dwindle";
-        resize_on_border = "true";
-      };
-
+      general = generalSettings;
       decoration = {
-        shadow = {
-          enabled = true;
-          range = 18;
-          render_power = 3;
-          color = lib.mkForce "rgba(00000066)";
-        };
+        shadow = shadowSettings;
         rounding = 5;
         dim_inactive = true;
         dim_strength = 0.05;
