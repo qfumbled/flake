a37a37d2677487ff64db933c48520f038775ae63
qfumbled <q.fumbled@proton.me>
2025-08-10 12:46:58 -0400

    Update theme.nix

--- home/laptop/hyprland/config/theme.nix
+++ home/laptop/hyprland/config/theme.nix
@@ -1,23 +1,21 @@
 { inputs, config, lib, ... }:
 
 let
-  # Define shadow settings in one place
-  shadowSettings = {
-    enabled = true;
-    range = 18;
-    render_power = 3;
-    # Force this color to avoid conflicting definitions
-    color = lib.mkForce "rgba(00000066)";
-  };
-
   generalSettings = with config.lib.stylix.colors; {
     gaps_in = 8;
     gaps_out = 12;
     border_size = 2;
-    "col.inactive_border" = "rgb(${base01})";
-    "col.active_border" = "rgb(${base01})";
+    "col.inactive_border" = lib.mkForce "rgb(66858f)";
+    "col.active_border" = lib.mkForce "rgb(66858f)";
     layout = "dwindle";
     resize_on_border = "true";
+  };
+
+  shadowSettings = {
+    enabled = true;
+    range = 18;
+    render_power = 3;
+    color = lib.mkForce "rgba(00000066)";
   };
 in
 {
