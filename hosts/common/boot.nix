f74c85dd3f4029c1f3bd238f9641baab3ae14347
qfumbled <qfumbled@proton.me>
2025-08-11 17:09:36 -0400

    fixes

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -1,24 +1,33 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   boot = {
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    # Define the kernel and related options
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    # Enable bootspec (e.g., specifying a bootloader)
     bootspec.enable = true;
 
     loader = {
+      # Enable systemd-boot as the bootloader
       systemd-boot.enable = true;
       efi.efiSysMountPoint = "/boot";
       efi.canTouchEfiVariables = true;
       timeout = 15;
     };
 
+    # Use tmpfs for /tmp
     tmp = {
       useTmpfs = true;
       cleanOnBoot = true;
     };
 
+    # Enable Plymouth for boot splash screen
     plymouth.enable = true;
+
+    # Kernel modules
     kernelModules = ["v4l2loopback"];
     extraModulePackages = [pkgs.linuxPackages_latest.v4l2loopback];
 
+    # Kernel parameters to pass during boot
     kernelParams = [
       "quiet"
       "splash"
@@ -32,15 +41,18 @@
       "intel_iommu=on"
       "amd_iommu=on"
       "amd_pstate=active"
-
       "ideapad_laptop.allow_v4_dytc=Y"
       "nvme_core.default_ps_max_latency_us=0"
     ];
 
+    # Configure initrd
     initrd.systemd.enable = true;
     initrd.verbose = false;
+
+    # Log levels
     consoleLogLevel = 3;
 
+    # Sysctl configurations (kernel parameters after boot)
     kernel.sysctl = {
       "net.isoc" = true;
       "kernel.kptr_restrict" = 1;
@@ -55,6 +67,7 @@
     };
   };
 
+  # Systemd service configuration for nix-daemon
   systemd.services.nix-daemon = {
     environment = {
       TMPDIR = "/var/tmp";
