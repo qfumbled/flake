f851a6b8d8333e16b20883f306c61b9a4e8677a5
qfumbled <qfumbled@proton.me>
2025-08-11 17:08:37 -0400

    Update boot.nix

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -1,59 +1,63 @@
-{ pkgs, ... }: {
-  boot.bootspec.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+{pkgs, ...}: {
+  boot = {
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    bootspec.enable = true;
 
-  tmp = {
-    useTmpfs = true;
-    cleanOnBoot = true;
+    loader = {
+      systemd-boot.enable = true;
+      efi.efiSysMountPoint = "/boot";
+      efi.canTouchEfiVariables = true;
+      timeout = 15;
+    };
+
+    tmp = {
+      useTmpfs = true;
+      cleanOnBoot = true;
+    };
+
+    plymouth.enable = true;
+    kernelModules = ["v4l2loopback"];
+    extraModulePackages = [pkgs.linuxPackages_latest.v4l2loopback];
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "fastboot"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+      "rootfstype=xfs"
+      "module.sig_enforce=1"
+      "lockdown=confidentiality"
+      "nvme.noacpi=1"
+      "intel_iommu=on"
+      "amd_iommu=on"
+      "amd_pstate=active"
+
+      "ideapad_laptop.allow_v4_dytc=Y"
+      "nvme_core.default_ps_max_latency_us=0"
+    ];
+
+    initrd.systemd.enable = true;
+    initrd.verbose = false;
+    consoleLogLevel = 3;
+
+    kernel.sysctl = {
+      "net.isoc" = true;
+      "kernel.kptr_restrict" = 1;
+      "kernel.dmesg_restrict" = 1;
+
+      "vm.swappiness" = 10;
+      "vm.vfs_cache_pressure" = 50;
+      "vm.dirty_ratio" = 10;
+      "vm.dirty_background_ratio" = 5;
+
+      "kernel.nmi_watchdog" = 0;
+    };
   };
 
-  plymouth.enable = true;
-
-  initrd.systemd.enable = true;
-  initrd.verbose = false;
-  consoleLogLevel = 3;
-
-  kernel.sysctl = {
-    "net.isoc" = true;
-    "kernel.kptr_restrict" = 1;
-    "kernel.dmesg_restrict" = 1;
-
-    "vm.swappiness" = 10;  # More aggressive swapping
-    "vm.vfs_cache_pressure" = 50;  # Adjusting virtual filesystem cache
-    "vm.dirty_ratio" = 10;  # Ratio of system memory used for dirty pages
-    "vm.dirty_background_ratio" = 5;  # Background dirty pages threshold
-
-    "kernel.nmi_watchdog" = 0;  # Disabling NMI watchdog
-  };
-
-  # Set TMPDIR for nix-daemon service
   systemd.services.nix-daemon = {
     environment = {
       TMPDIR = "/var/tmp";
     };
   };
-
-  # Kernel boot parameters
-  kernelParams = [
-    "quiet"  # Suppress kernel messages during boot
-    "splash"  # Show splash screen (Plymouth)
-    "fastboot"  # Speed up the boot process
-    "udev.log_priority=3"  # Log udev events with normal priority
-    "rd.systemd.show_status=auto"  # Show systemd status during boot
-    "rootfstype=xfs"  # Set root filesystem type to XFS
-    "module.sig_enforce=1"  # Enforce module signature checking
-    "lockdown=confidentiality"  # Enforce lockdown mode to secure the kernel
-    "nvme.noacpi=1"  # Disable ACPI for NVMe devices (useful for some systems)
-    "intel_iommu=on"  # Enable Intel IOMMU for PCI passthrough (useful for VMs)
-    "amd_iommu=on"  # Enable AMD IOMMU (useful for VMs)
-    "amd_pstate=active"  # Force AMD power state driver to active mode for performance
-
-    "ideapad_laptop.allow_v4_dytc=Y"  # Enable dynamic thermal control for Lenovo Ideapad laptops
-    "nvme_core.default_ps_max_latency_us=0"  # Set maximum power state latency for NVMe drives to 0
-  ];
-
-  # EFI System Partition mount point
-  efi.efiSysMountPoint = "/boot";  # Mount point for the EFI partition
 }
