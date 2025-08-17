ebf7607f9a46a71505512ce2f96789747c0e10f6
wug <qfumbled@proton.me>
2025-08-17 10:24:18 -0400

    now

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./bluetooth
     ./graphics
-    ./niri
+    ./hyprland
     ./qt
     ./system
     ./wg-client
--- modules/nixos/niri/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri-unstable;
-  };
-  programs.xwayland.enable = true;
-}
