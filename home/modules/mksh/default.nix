904bccfd4799511e1563321d65201302f9c3d759
wug <rs.wug@proton.me>
2025-09-11 20:36:18 -0400

    breaking laws of nix

--- home/modules/mksh/default.nix
+++ home/modules/mksh/default.nix
@@ -1,28 +1,17 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }:
-
 let
-  fzfOpts = with config.lib.stylix.colors; {
-    export = {
-      FZF_DEFAULT_OPTS = ''
-        $FZF_DEFAULT_OPTS \
-        --color=fg:#${base04},bg:#${base00},hl:#${base0D} \
-        --color=fg+:#${base06},bg+:#${base01},hl+:#${base0D} \
-        --color=info:#${base0D},prompt:#${base0D},pointer:#${base0C} \
-        --color=marker:#${base0C},spinner:#${base0C},header:#${base0D}
-      '';
-    };
-  };
+  fzfOpts = with config.lib.stylix.colors; 
+    "export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#${base04},bg:#${base00},hl:#${base0D} --color=fg+:#${base06},bg+:#${base01},hl+:#${base0D} --color=info:#${base0D},prompt:#${base0D},pointer:#${base0C} --color=marker:#${base0C},spinner:#${base0C},header:#${base0D}'";
 in
 {
   home.packages = with pkgs; [
     mksh
     fd
-    fzf
-    eza
   ];
 
   programs.fzf.enable = true;
@@ -35,24 +24,20 @@ in
   home.file.".mkshrc".text = ''
     # vim: ft=sh
 
+    alias l='eza -lh'
+    alias ls='eza -1'
+    alias ll='eza -lha --sort=name --group-directories-first'
+    alias ld='eza -lhD'
+    alias yz='yazi'
+    alias dev='nix develop -c tmux'
+    alias vg='valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt'
+
     HISTFILE=$HOME/.mksh_history
     HISTSIZE=5000
     HISTCONTROL=ignoreboth
 
     ${fzfOpts}
 
-    # Aliases
-    aliases = {
-      l   = "eza -lh";
-      ls  = "eza -1";
-      ll  = "eza -lha --sort=name --group-directories-first";
-      ld  = "eza -lhD";
-      yz  = "yazi";
-      dev = "nix develop -c tmux";
-      vg  = "valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt";
-    };
-
-    # Custom cd function
     cd() {
       local target="$1"
       local parent
@@ -83,24 +68,23 @@ in
       fi
     }
 
-    # Fuzzy directory changer
     cdd() {
       local dir
       dir=$(fd -H -t d -a | fzf)
       [ -n "$dir" ] && cd "$dir"
     }
 
-    # Go up n directories (default 1)
     ..() {
       local n=1
       local i
       [ -n "$1" ] && n="$1"
       for i in $(seq 1 "$n"); do
-        cd ..
+          cd ..
       done
     }
 
     set -o vi
     cd "$PWD"
   '';
+}
 }
